b0256cde922bf0b3a87fef0bac638ec2f0a4e08f
Jim89 <jimmy22theave@googlemail.com>
2015-03-13 12:01:16 +0000

    Changed journey time line charts to run from 1 unified function

--- oysterAnalysis.R
+++ oysterAnalysis.R
@@ -18,7 +18,7 @@
   source("./gettingAndCleaning/02_stationsData.R")
   source("./gettingAndCleaning/03_combined.R")
   print("Complete, took:")
-  proc.time() - ptm;
+  proc.time() - ptm
 
 # set up some colours to use
   districtLine <- col2rgb("#007229")
@@ -30,10 +30,12 @@
 journeyTimeHist <-
 combined %>%
   ggplot(aes(x = journey.time %>% as.numeric)) +
-  geom_histogram(binwidth = 5, aes(fill = weekend), alpha = 0.8, colour = "lightgrey") +
+  geom_histogram(binwidth = 5, aes(fill = weekend), alpha = 0.8, 
+                 colour = "lightgrey") +
   facet_grid(weekend ~ ., scales = "fixed") +
   scale_x_continuous(breaks = seq(from = 0,
-                                  to = max(combined$journey.time)%>% as.numeric + 5,
+                                  to = max(combined$journey.time) %>% 
+                                       as.numeric + 5,
                                   by = 5)) +
   scale_fill_manual(values = rep(kpmgDarkBlue, 2), guide = F) +
   xlab("Journey time / minutes") +
@@ -42,7 +44,8 @@ combined %>%
         #axis.text.y = element_blank(),
         text = element_text(size = 14),
         panel.grid.minor.x = element_blank(),
-        panel.grid.major.x = element_line(colour = "lightgrey", linetype = "dotted"),
+        panel.grid.major.x = element_line(colour = "lightgrey", 
+                                          linetype = "dotted"),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
         panel.margin.y = unit(0.1, units = "in"),
@@ -50,81 +53,55 @@ combined %>%
 
 
 
-# line chart of journey times accross morning commute --------------------------
-  start <- "06:30:00" %>% strptime(format = "%T") %>% as.POSIXct
-  end <- "08:00:00" %>% strptime(format = "%T") %>% as.POSIXct
+# line chart of journey times for my commute -----------------------------------
+# create function to plot the data (as I'll do it for both morning and evening)
+CommutePlot <- function(data, start, end) {
+  data %>%
+    filter(weekend != "Weekend") %>%
+    mutate(start.time.clean = start.time.clean %>% as.character %>%
+             str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]") %>%
+             strptime(format = "%T") %>%
+             as.POSIXct %>%
+             CeilingTime(2, "minute")) %>%
+    filter(start.time.clean %>% between(start, end)) %>%
+    group_by(start.time.clean) %>%
+    summarise(journeys = n(),
+              journey.time = journey.time %>% as.numeric %>% mean) %>% # View
+    mutate(start.time.clean = start.time.clean %>% as.character %>% 
+             str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]")) %>%
+    ggplot(aes(x = start.time.clean, y = journey.time, group = 1)) +
+    geom_line() +
+    geom_point(aes(size = journeys), colour = kpmgDarkBlue, alpha = 0.8) +
+    scale_size(name = "Number of\nJourneys",
+               range = c(0, 10)) +
+    xlab("Departure Time") +
+    ylab("Journey Time / minutes") +
+    #  geom_smooth(method = "lm", alpha = 0.075) +
+    geom_smooth(method = "loess", size = 0.5, 
+                colour = kpmgDarkBlue, alpha = 0.075) +
+    theme(legend.position = "bottom",
+          axis.text.x = element_text(angle = -90),
+          axis.text.y = element_text(size = 12),
+          #axis.title.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          panel.grid.major.x = element_line(colour = "lightgrey", 
+                                            linetype = "dotted"),
+          panel.grid.major.y = element_line(colour = "lightgrey", 
+                                            linetype = "dotted"),
+          panel.grid.minor.y = element_blank(),
+          panel.margin.y = unit(0.1, units = "in"),
+          panel.background = element_rect(fill = "white",colour = "lightgrey"),
+          legend.background = element_rect(fill = "white")) 
+}
 
-morningCommute <-
-combined %>%
-  filter(weekend != "Weekend") %>%
-  mutate(start.time.clean = start.time.clean %>% as.character %>%
-           str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]") %>%
-           strptime(format = "%T") %>%
-           as.POSIXct %>%
-           CeilingTime(2, "minute")) %>%
-  filter(start.time.clean %>% between(start, end)) %>%
-  group_by(start.time.clean) %>%
-  summarise(journeys = n(),
-            journey.time = journey.time %>% as.numeric %>% mean) %>%
- mutate(start.time.clean = start.time.clean %>% as.character %>% str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]")) %>%
-  ggplot(aes(x = start.time.clean, y = journey.time, group = 1)) +
-  geom_line() +
-  geom_point(aes(size = journeys), colour = kpmgDarkBlue, alpha = 0.8) +
-  scale_size(name = "Number of\nJourneys",
-             range = c(0, 10)) +
-  xlab("Departure Time") +
-  ylab("Journey Time / minutes") +
-#  geom_smooth(method = "lm", alpha = 0.075) +
-  geom_smooth(method = "loess", size = 0.5, colour = kpmgDarkBlue, alpha = 0.075) +
-  theme(legend.position = "bottom",
-        axis.text.x = element_text(angle = -90),
-        axis.text.y = element_text(size = 12),
-        axis.title.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        panel.grid.major.x = element_line(colour = "lightgrey", linetype = "dotted"),
-        panel.grid.major.y = element_line(colour = "lightgrey", linetype = "dotted"),
-        panel.grid.minor.y = element_blank(),
-        panel.margin.y = unit(0.1, units = "in"),
-        panel.background = element_rect(fill = "white",colour = "lightgrey"),
-        legend.background = element_rect(fill = "white"))
+# set up start and end time windows
+  startMorning <- "06:30:00" %>% strptime(format = "%T") %>% as.POSIXct
+  endMorning <- "08:00:00" %>% strptime(format = "%T") %>% as.POSIXct
 
+  startEvening <- "17:15:00" %>% strptime(format = "%T") %>% as.POSIXct
+  endEvening <- "19:00:00" %>% strptime(format = "%T") %>% as.POSIXct
 
-# line chart of journey times accross evening commute --------------------------
-  start <- "17:00:00" %>% strptime(format = "%T") %>% as.POSIXct
-  end <- "19:00:00" %>% strptime(format = "%T") %>% as.POSIXct
-
-eveningCommute <-
-combined %>%
-  filter(weekend != "Weekend") %>%
-  mutate(start.time.clean = start.time.clean %>% as.character %>%
-           str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]") %>%
-           strptime(format = "%T") %>%
-           as.POSIXct %>%
-           CeilingTime(2, "minute")) %>%
-  filter(start.time.clean %>% between(start, end)) %>%
-  group_by(start.time.clean) %>%
-  summarise(journeys = n(),
-            journey.time = journey.time %>% as.numeric %>% mean) %>% # View
- mutate(start.time.clean = start.time.clean %>% as.character %>% str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]")) %>%
-  ggplot(aes(x = start.time.clean, y = journey.time, group = 1)) +
-  geom_line() +
-  geom_point(aes(size = journeys), colour = kpmgDarkBlue, alpha = 0.8) +
-  scale_size(name = "Number of\nJourneys",
-             range = c(0, 10)) +
-  xlab("Departure Time") +
-  ylab("Journey Time / minutes") +
-#  geom_smooth(method = "lm", alpha = 0.075) +
-  geom_smooth(method = "loess", size = 0.5, colour = kpmgDarkBlue, alpha = 0.075) +
-  theme(legend.position = "bottom",
-        axis.text.x = element_text(angle = -90),
-        axis.text.y = element_text(size = 12),
-        axis.title.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        panel.grid.major.x = element_line(colour = "lightgrey", linetype = "dotted"),
-        panel.grid.major.y = element_line(colour = "lightgrey", linetype = "dotted"),
-        panel.grid.minor.y = element_blank(),
-        panel.margin.y = unit(0.1, units = "in"),
-        panel.background = element_rect(fill = "white",colour = "lightgrey"),
-        legend.background = element_rect(fill = "white"))
+# create the plots
+  morningCommute <- CommutePlot(combined, startMorning, endMorning)
+  eveningCommute <- CommutePlot(combined, startEvening, endEvening)
