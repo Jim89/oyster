05dc76dbf41541a0de91b20cddd4b991e3cb1b1b
Jim Leach <jim.leach@kpmg.co.uk>
2015-06-04 10:32:12 +0100

    Clean, semi-finalised version of working (although v simple) Shiny app for exploring Oyster card data. Saving as a clean commit so that I can roll back to this if I balls it all up later

--- shiny/server.R
+++ shiny/server.R
@@ -53,6 +53,48 @@
         panel.background = element_rect(fill = "white", colour = "lightgrey"))
   }
 
+# create function for plotting commute
+  CommutePlot <- function (data, start, end, interval) {
+  # take the data and perform some manipulations
+  data %>%
+    filter(weekend != "Weekend") %>%
+    mutate(start.time.clean = start.time.clean %>% as.character %>%
+             str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]") %>%
+             strptime(format = "%T") %>%
+             as.POSIXct %>%
+             CeilingTime(interval, "minute")) %>%
+    filter(start.time.clean %>% between(start, end)) %>%
+    group_by(start.time.clean) %>%
+    summarise(journeys = n(),
+              journey.time = journey.time %>% as.numeric %>% mean) %>% # View
+    mutate(start.time.clean = start.time.clean %>% as.character %>%
+             str_extract("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]")) %>%
+  # make the plot itself
+    ggplot(aes(x = start.time.clean, y = journey.time, group = 1)) +
+  #  geom_line(colour = kpmgDarkBlue) +
+    geom_point(aes(size = journeys), colour = kpmgDarkBlue, alpha = 0.8) +
+    scale_size(name = "Number of\nJourneys", range = c(0, 10)) +
+    xlab("Departure Time") +
+    ylab("Average Journey Time / minutes") +
+    #  geom_smooth(method = "lm", alpha = 0.075) +
+#   geom_smooth(method = "loess", size = 0.5, colour = kpmgPurple, alpha = 0.25) +
+    theme(legend.position = "bottom",
+          axis.text.x = element_text(angle = 90),
+          axis.text.y = element_text(size = 12),
+          #axis.title.y = element_blank(),
+          #axis.ticks.y = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          panel.grid.major.x = element_blank(),
+  #           element_line(colour = "lightgrey",s
+  #                                           linetype = "dotted"),
+          panel.grid.major.y = element_line(colour = "lightgrey",
+                                            linetype = "dotted"),
+          panel.grid.minor.y = element_blank(),
+          panel.margin.y = unit(0.1, units = "in"),
+          panel.background = element_rect(fill = "white",colour = "lightgrey"),
+          legend.background = element_rect(fill = "white"))
+  }
+
 
 # set up data for the map widget -----------------------------------------------
 # reshape the data
@@ -100,6 +142,36 @@ shinyServer( function(input, output) {
                                                        note)) %>%
                                   select(-note) %>%
                                   setNames(c("Date", "Touched in", "Touched Out",
-                                             "Journey", "Charge (£)", "Note"))})
+                                             "Journey", "Charge", "Note"))})
 
+  # create the commute plot
+    output$commute <- renderPlot({
+      if ( input$commute == "Morning"){
+        # set up start and end time windows
+          start <- "06:30:00" %>% strptime(format = "%T") %>% as.POSIXct
+          end <- "08:00:00" %>% strptime(format = "%T") %>% as.POSIXct
+          if (input$smooth == TRUE) {
+            CommutePlot(combined, start, end, 2) +
+            geom_smooth(method = "loess",
+                        size = 0.5,
+                        colour = kpmgPurple,
+                        alpha = 0.25)
+          } else {
+            CommutePlot(combined, start, end, 2)
+          }
+      } else if ( input$commute == "Evening") {
+        # set up start and end windows
+          start <- "17:00:00" %>% strptime(format = "%T") %>% as.POSIXct
+          end <- "19:00:00" %>% strptime(format = "%T") %>% as.POSIXct
+          if (input$smooth == TRUE) {
+            CommutePlot(combined, start, end, 2) +
+            geom_smooth(method = "loess",
+                        size = 0.5,
+                        colour = kpmgPurple,
+                        alpha = 0.25)
+          } else {
+            CommutePlot(combined, start, end, 2)
+          }
+      }
+   })
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,5 +1,6 @@
 library(shiny)
-
+library(leaflet)
+library(DT)
 
 # Set up the application
 shinyUI(fluidPage(
@@ -7,12 +8,21 @@ shinyUI(fluidPage(
     titlePanel("TfL Oyster Card Data Explorations"),
 
   # set up the sidebar
-    sidebarLayout(
+    #sidebarLayout(position = "left",
       sidebarPanel(
-        radioButtons("day", "Weekends or Weekdays?\n (affects histogram only)",
-                     c("Both" = "Both",
-                       "Weekday" = "Weekday",
-                       "Weekend" = "Weekend"))
+        h4("Weekends or weekdays?"),
+        helpText("Chose whether you would like to see a histogram of journey time for weekdays, weekends, or both."),
+        radioButtons("day", NULL,
+                     c("Weekday" = "Weekday",
+                       "Weekend" = "Weekend",
+                       "Both" = "Both")),
+        br(),
+        h4("Morning or evening commute?"),
+        helpText("Find the optimum commute time - chose whether you would like to see a plot of commute duration vs. touch in time for mornings or evenings (smoother optional)"),
+        radioButtons("commute", NULL,
+                     c("Morning" = "Morning",
+                       "Evening" = "Evening")),
+        checkboxInput('smooth', 'Smooth', FALSE)
       ),
 
   # add the main panel
@@ -20,8 +30,11 @@ shinyUI(fluidPage(
       tabsetPanel(type = "tabs",
                   tabPanel("Map", leafletOutput("map")),
                   tabPanel("Journey Time Histogram", plotOutput("hist")),
+                  tabPanel("Commute time", plotOutput("commute")),
                   tabPanel("The Data", dataTableOutput("data"))
                   )
     )
-    )
+    #)
 ))
+
+
