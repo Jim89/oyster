046832646f632d2570a078a3e819302b4587db45
Jim Leach <jimmy22theave@gmail.com>
2015-09-23 16:44:18 +0100

    moved data

---
+++ sql/001_create_import_stations.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS stations.locations;
+CREATE TABLE stations.locations (
+STATION		VARCHAR(100)
+,LAT		numeric(10,8)
+,LON		numeric(10,8)
+);
+COPY stations.locations(station, lat, lon) FROM 'C:\Users\Jleach1\Documents\oyster\data\stations.csv' DELIMITER ',' CSV;
